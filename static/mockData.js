3c5b930519f318126c4eaaa10a6944fc569d1720
Chris Cantu <pinion31@gmail.com>
2017-10-17 16:09:35 -0500

    initial event page setup

---
+++ static/mockData.js
@@ -0,0 +1,40 @@
+var myEvent =
+
+{
+    "0": {
+        "created": 1404011429000,
+        "id": "242931161",
+        "name": "Psychology of Fake News & Pivoting to a New Career",
+        "status": "past",
+        "time": 1507680000000,
+        "updated": 1507692921000,
+        "utc_offset": -18000000,
+        "waitlist_count": 0,
+        "yes_rsvp_count": 119,
+        "venue": {
+            "id": 12558312,
+            "name": "The North Door ",
+            "lat": 30.264577865600586,
+            "lon": -97.7339859008789,
+            "repinned": false,
+            "address_1": "502 Brushy St. ",
+            "city": "Austin",
+            "country": "us",
+            "localized_country_name": "USA",
+            "zip": "78702",
+            "state": "TX"
+        },
+        "group": {
+            "created": 1178223728000,
+            "name": "Refresh Austin (Web Design & Developer Meetup)",
+            "id": 492413,
+            "join_mode": "open",
+            "lat": 30.270000457763672,
+            "lon": -97.73999786376953,
+            "urlname": "Austin-Web-Design",
+            "who": "Refreshers",
+            "localized_location": "Austin, TX",
+            "region": "en_US"
+        }
+        }
+};
